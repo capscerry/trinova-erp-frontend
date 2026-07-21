84e9a7f57a963cc51f5f590b512d4c65f2f44bf6
vikoadrian32 <vikosilaban1@gmail.com>
2026-07-21 08:00:44 +0700

    update page quotation

--- app/penjualan/quotation/page.tsx
+++ app/penjualan/quotation/page.tsx
@@ -352,7 +352,7 @@ export default function SalesQuotationPage() {
           </div>
 
           <div className="flex items-center gap-2">
-            <button
+            {/* <button
               className="inline-flex items-center gap-1 px-3 py-2 text-xs font-semibold
                          text-slate-600 bg-white border border-slate-200 rounded-lg hover:bg-slate-50 transition-colors"
             >
@@ -364,7 +364,7 @@ export default function SalesQuotationPage() {
                          border-slate-200 text-slate-500 hover:bg-slate-100 transition-colors"
             >
               <Printer size={13} />
-            </button>
+            </button> */}
 
             <div className="relative">
               <Search
